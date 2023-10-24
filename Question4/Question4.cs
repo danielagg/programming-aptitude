28ffe72cc4fd4ef7b5ba05223762a970b1a0cc5b
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 20:06:36 +0200

    question3: init, handle nulls

---
+++ Question4/Question4.cs
@@ -0,0 +1,20 @@
+namespace Programming_Aptitude.Question4;
+
+public static class Question4
+{
+    public record TreeNode<T>(T Value)
+    {
+        public TreeNode<T>? Left { get; init; }
+        public TreeNode<T>? Right { get; init; }
+    }
+    
+    public static List<TreeNode<T>> GetParentNodesOfSingleLeaves<T>(TreeNode<T>? root)
+    {
+        var result = new List<TreeNode<T>>();
+      
+        if (root is null)
+            return result;
+
+        return default; // todo
+    }
+}
--- Question4/Test.cs
+++ Question4/Test.cs
@@ -1,6 +1,16 @@
+using FluentAssertions;
+using Xunit;
+using TreeNode = Programming_Aptitude.Question4.Question4.TreeNode<int>;
+
 namespace Programming_Aptitude.Question4;
 
 public class Test
 {
-    
+    [Fact]
+    public void GivenNull_GetParentNodesOfSingleLeaves_ReturnsEmptyResult()
+    {
+        Question4.GetParentNodesOfSingleLeaves<int>(null)
+            .Should()
+            .BeEmpty();
+    }
 }
