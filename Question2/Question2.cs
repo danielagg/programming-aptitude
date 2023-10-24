f03c8668d1c2892dbbfe3f0d0d8e7db1d7d1bf7f
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 19:38:41 +0200

    question2: iterative option, reverse list

--- Question2/Question2.cs
+++ Question2/Question2.cs
@@ -4,7 +4,15 @@ public static class Question2
 {
     public static LinkedList<T>? ReverseLinkedListWithIteration<T>(LinkedList<T>? list)
     {
-        return null;
+        if (list?.First is null)
+            return null;
+        
+        var reversedList = new LinkedList<T>();
+
+        foreach (var item in list)
+            reversedList.AddFirst(item);
+
+        return reversedList;
     }
     
     // public static LinkedList<T>? ReverseLinkedListWithRecursion<T>(LinkedList<T>? list)
--- Question2/TestIterativeReversal.cs
+++ Question2/TestIterativeReversal.cs
@@ -14,4 +14,26 @@ public class TestIterativeReversal
             .Should()
             .BeNull();
     }
+    
+    [Fact]
+    public void GivenLinkedListWithSingleEntry_ReverseLinkedListWithIteration_ReturnsLinkedListWithSameSingleEntry()
+    {
+        var input = new LinkedList<int>(new[] { 1 });
+        var expectedOutput = new LinkedList<int>(new[] { 1 });
+        
+        Question2.ReverseLinkedListWithIteration(input)
+            .Should()
+            .BeEquivalentTo(expectedOutput);
+    }
+    
+    [Fact]
+    public void GivenLinkedListWithMultipleEntries_ReverseLinkedListWithIteration_ReturnsReversedLinkedList()
+    {
+        var input = new LinkedList<int>(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        var expectedOutput = new LinkedList<int>(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+        
+        Question2.ReverseLinkedListWithIteration(input)
+            .Should()
+            .BeEquivalentTo(expectedOutput);
+    }
 }
