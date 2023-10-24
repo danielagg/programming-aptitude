ea382889cc4d1a9b160b47c70bf3d7086f8293f1
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 19:03:06 +0200

    question1: use GenericList<T>

--- Question1/Question1.cs
+++ Question1/Question1.cs
@@ -4,8 +4,8 @@ public static class Question1
 {
     public static void Main(string[] args)
     {
-        AnimalList listOfAnimals = new AnimalList();
-        DoubleList listOfNumbers = new DoubleList();
+        var listOfAnimals = new GenericList<Animal>();
+        var listOfNumbers = new GenericList<double>();
         
         listOfNumbers.DoSomething(2.5);
         listOfAnimals.DoSomething(new Animal());
@@ -14,21 +14,21 @@ public static class Question1
     public class Animal
     {
         public string Name { get; set; }
-        public List<string> Habitats { get; set; }
+        public GenericList<string> Habitats { get; set; }
     }
     
-    public class AnimalList
+    public class GenericList<T>
     {
-        public void DoSomething(Animal animal)
+        public void DoSomething(T item) => Console.WriteLine($"Foo {GetToPrint(item)}");
+        
+        // in the example, when called from DoubleList, even though 25 was passed along, DoSomething() printed "Number"
+        // so, I've assumed that the type of the item is what matters, not the value
+        // todo? or we should have just returned item.GetType().Name?
+        private static string GetToPrint(T item) => item switch
         {
-            Console.WriteLine("Foo Animal");
-        }
-    }
-    public class DoubleList
-    {
-        public void DoSomething(double number)
-        {
-            Console.WriteLine("Foo Number");
-        }
+            double => "Number",
+            Animal => "Animal",
+            _ => item?.GetType().Name ?? "Unknown"
+        };
     }
 }
