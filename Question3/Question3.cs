703a8583316fb0e816afdf9a36cc6c54fb3393f2
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 19:54:12 +0200

    question3: init, null/whitespace checks

---
+++ Question3/Question3.cs
@@ -0,0 +1,15 @@
+namespace Programming_Aptitude.Question3;
+
+public static class Question3
+{
+    public static bool AreAnagrams(string? word1, string? word2)
+    {
+        if (word1 is null || word2 is null)
+            return false;
+        
+        if(word1 == string.Empty || word2 == string.Empty)
+            return false;
+
+        return false; // todo
+    }
+}
--- Question3/Test.cs
+++ Question3/Test.cs
@@ -1,6 +1,32 @@
+using FluentAssertions;
+using Xunit;
+
 namespace Programming_Aptitude.Question3;
 
 public class Test
 {
+    [Fact]
+    public void GivenTwoNulls_AreAnagrams_ReturnsFalse()
+    {
+        Question3.AreAnagrams(null, null).Should().BeFalse();
+    }
     
+    [Fact]
+    public void GivenNullAndEmptyString_AreAnagrams_ReturnsFalse()
+    {
+        Question3.AreAnagrams(string.Empty, null).Should().BeFalse();
+        Question3.AreAnagrams(null, string.Empty).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void GivenTwoEmptyStrings_AreAnagrams_ReturnsFalse()
+    {
+        Question3.AreAnagrams(string.Empty, string.Empty).Should().BeFalse();
+    }
+    
+    [Fact]
+    public void GivenWhiteSpaces_AreAnagrams_ReturnsFalse()
+    {
+        Question3.AreAnagrams("   ", " ").Should().BeFalse();
+    }
 }
