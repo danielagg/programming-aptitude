7e20aa9737b11bbc64d42c5b5e4562e17d0378ac
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 20:02:27 +0200

    question3: add proper anagram-finder logic, tests

--- Question3/Question3.cs
+++ Question3/Question3.cs
@@ -4,12 +4,55 @@ public static class Question3
 {
     public static bool AreAnagrams(string? word1, string? word2)
     {
+        /* Assumptions:
+         - null and empty strings are *not* considered as anagrams
+         - whitespaces (leading, trailing and within the words) are to be ignored
+         - anagrams are case insensitive (eg. Word and word are anagrams)
+         - input words can contain numbers (eg. 123 and 321 are anagrams)
+         - input words can contain any UTF-16 character (eg. ϻϱǾ˅ and ˅Ǿϱϻ are anagrams)
+        */
         if (word1 is null || word2 is null)
             return false;
+
+        // space complexity: we're creating new strings, n + m
+        word1 = word1.Replace(" ", string.Empty).ToLower();
+        word2 = word2.Replace(" ", string.Empty).ToLower();
         
         if(word1 == string.Empty || word2 == string.Empty)
             return false;
+        
+        if (word1.Length != word2.Length)
+            return false;
 
-        return false; // todo
+        // for both words, we figure out how many times each unique character appears
+        // time complexity is n+m
+        var word1LetterFrequency = GetFrequencyOfUniqueLetters(word1);
+        var word2LetterFrequency = GetFrequencyOfUniqueLetters(word2);
+
+        foreach (var characterFromWord1 in word1LetterFrequency.Keys)
+        {
+            if (!word2LetterFrequency.ContainsKey(characterFromWord1))
+                return false;
+            
+            if (word1LetterFrequency[characterFromWord1] != word2LetterFrequency[characterFromWord1])
+                return false;
+        }
+
+        return true;
+    }
+    
+    private static Dictionary<char, int> GetFrequencyOfUniqueLetters(string word)
+    {
+        var frequency = new Dictionary<char, int>();
+        
+        foreach (var character in word)
+        {
+            if(!frequency.ContainsKey(character)) // seems faster than TryAdd()
+                frequency.Add(character, 1);
+            else
+                frequency[character]++;
+        }
+        
+        return frequency;
     }
 }
--- Question3/Test.cs
+++ Question3/Test.cs
@@ -29,4 +29,25 @@ public class Test
     {
         Question3.AreAnagrams("   ", " ").Should().BeFalse();
     }
+    
+    [Theory]
+    [InlineData("anagram", "nagaram")]
+    [InlineData(" anagram", "nagaram")]
+    [InlineData("a nagram", "nagaram")]
+    [InlineData("1234567890", "0123456789")]
+    [InlineData("WoRd", "wOrD")]
+    [InlineData("ϻϱǾ\u02c5", "\u02c5Ǿϱϻ")]
+    public void GivenTwoWordsWhichAreAnagrams_AreAnagrams_ReturnsTrue(string word1, string word2)
+    {
+        Question3.AreAnagrams(word1, word2).Should().BeTrue();
+    }
+    
+    [Theory]
+    [InlineData("car", "tar")]
+    [InlineData("t", "tar")]
+    [InlineData("tar", "32Ǿ")]
+    public void GivenTwoWordsWhichAreNotAnagrams_AreAnagrams_ReturnsFalse(string word1, string word2)
+    {
+        Question3.AreAnagrams(word1, word2).Should().BeFalse();
+    }
 }
