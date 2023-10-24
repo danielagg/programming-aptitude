3182ec78c8441f16466a38ca39788d56bb8c8d80
Daniel Agg <daniel.agg@outlook.com>
2023-10-24 20:15:01 +0200

    question4: multiple node cases

--- Question4/Question4.cs
+++ Question4/Question4.cs
@@ -14,7 +14,35 @@ public static class Question4
       
         if (root is null)
             return result;
+      
+        GetParentNodesOfSingleLeavesRecursively(root, result);
+      
+        Console.WriteLine($"Parent nodes of single leaves: {string.Join(", ", result.Select(node => node.Value))}");
 
-        return new List<TreeNode<T>>();
+        return result;
+    }
+    
+    private static void GetParentNodesOfSingleLeavesRecursively<T>(TreeNode<T>? node, ICollection<TreeNode<T>> result)
+    {
+        switch (node)
+        {
+            case null:
+                return;
+         
+            case { Left: not null, Right: null } or { Left: null, Right: not null }:
+            {
+                // on 'node', either left or right exist, never both
+                var child = node.Left ?? node.Right;
+
+                // check if single 'child' is a leaf --> then parent (node) is a match
+                if (child?.Left is null && child?.Right is null)
+                    result.Add(node);
+
+                break;
+            }
+        }
+
+        GetParentNodesOfSingleLeavesRecursively(node.Left, result);
+        GetParentNodesOfSingleLeavesRecursively(node.Right, result);
     }
 }
--- Question4/Test.cs
+++ Question4/Test.cs
@@ -21,4 +21,103 @@ public class Test
             .Should()
             .BeEmpty();
     }
+    
+    [Fact]
+    public void GivenOneParentOneLeafNode_GetParentNodesOfSingleLeaves_ReturnsRootNode()
+    {
+        Question4.GetParentNodesOfSingleLeaves(new TreeNode(50) { Right = new TreeNode(51) })
+            .Should()
+            .BeEquivalentTo(new[] { new TreeNode(50) { Right = new TreeNode(51) } }.ToList());
+    }
+    
+    [Fact]
+    public void GivenOneParentTwoLeafNodes_GetParentNodesOfSingleLeaves_ReturnsEmptyResult()
+    {
+        var root = new TreeNode(50)
+        {
+            Left = new TreeNode(51),
+            Right = new TreeNode(52)
+        };
+        
+        Question4.GetParentNodesOfSingleLeaves(root)
+            .Should()
+            .BeEmpty();
+    }
+    
+    [Fact]
+    public void GivenExample1BinaryTree_GetParentNodesOfSingleLeaves_ReturnsParentNodesOfSingleLeaves()
+    {
+        var root = new TreeNode(50)
+        {
+            Left = new TreeNode(17)
+            {
+                Left = new TreeNode(9)
+                {
+                    Right = new TreeNode(14)
+                    {
+                        Left = new TreeNode(12)
+                    }
+                },
+                Right = new TreeNode(23)
+                {
+                    Right = new TreeNode(19)
+                }
+            },
+            Right = new TreeNode(76)
+            {
+                Left = new TreeNode(54)
+                {
+                    Right = new TreeNode(72)
+                    {
+                        Left = new TreeNode(67)
+                    }
+                }
+            }
+        };
+
+        Question4.GetParentNodesOfSingleLeaves(root)
+            .Should()
+            .BeEquivalentTo(new[]
+            {
+                new TreeNode(14) { Left = new TreeNode(12) },
+                new TreeNode(23) { Right = new TreeNode(19) },
+                new TreeNode(72) { Left = new TreeNode(67) },
+            }.ToList());
+    }
+    
+    [Fact]
+    public void GivenExample2BinaryTree_GetParentNodesOfSingleLeaves_ReturnsParentNodesOfSingleLeaves()
+    {
+        var root = new TreeNode(1)
+        {
+            Left = new TreeNode(2)
+            {
+                Left = new TreeNode(4),
+                Right = new TreeNode(5)
+                {
+                    Left = new TreeNode(6)
+                    {
+                        Left = new TreeNode(8)
+                    },
+                    Right = new TreeNode(7)
+                    {
+                        Right = new TreeNode(9)
+                    }
+                }
+            },
+            Right = new TreeNode(3)
+            {
+                Left = new TreeNode(10)
+            }
+        };
+        
+        Question4.GetParentNodesOfSingleLeaves(root)
+            .Should()
+            .BeEquivalentTo(new[]
+            {
+                new TreeNode(6) { Left = new TreeNode(8) },
+                new TreeNode(7) { Right = new TreeNode(9) },
+                new TreeNode(3) { Left = new TreeNode(10) }
+            });
+    }
 }
